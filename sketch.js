21318f2ef683c787827a484c47ed9bcb366c5e16
FIRST_NAME LAST_NAMEKate <kec22@fsu.edu>
2023-10-02 10:19:15 -0400

    initial

--- sketch.js
+++ sketch.js
@@ -1,7 +1,42 @@
+let product
+const api_url = "https://fakestoreapi.com/"
+
 function setup() {
   createCanvas(400, 400);
+
+  fetchProduct();
+
+  let buyButton = createButton('Buy Now');
+  buyButton.position(150, height+10);
+  buyButton.mousePressed(buyProduct)
 }
 
+
+function fetchProduct() {
+  fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+} 
+
+
 function draw() {
-  background(125);
+  background(150);
+
+  textAlign(CENTER)
+  textSize(16)
+
+  if (product) { 
+text('Title: ${product.title}');
+text('Price: ${product.price}');
+  } else{
+    text("Error fetching product");
+  }
 }
+
+
+function buyProduct() {
+  window.alert('are you sure? What about this one instead?')
+
+fetchProduct()
+}
+
