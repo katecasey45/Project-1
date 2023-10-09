4ba3ea511ee89803595536eeda414e14977e9cc3
FIRST_NAME LAST_NAMEKate <kec22@fsu.edu>
2023-10-09 09:17:50 -0400

    10/9 - popups work

--- sketch.js
+++ sketch.js
@@ -1,41 +1,7 @@
-// let json
-// const api_url = "https://fakestoreapi.com/"
-
-// function preload() {
-//   json = loadJSON("https://fakestoreapi.com/products");
-// }
-
-// function setup() {
-//   noCanvas();
-
-//   let d1 = createDiv();
-// d1.position(0,0);
-//   let b = createButton("Buy Now");
-//   b.mouseClicked( clicked );
-//   console.log(json[0]);
-//   createP(json[0].title);
-//   img = createImg(json[0].image);
-//   img.style("width","200px");
-//   createP(json[0].price);
-
-//   let b2 = createButton("Buy Now");
-//   b2.mouseClicked( clicked );
-//   console.log(json[1]);
-//   createP(json[1].title);
-//   img = createImg(json[1].image);
-//   img.style("width","200px");
-//   createP(json[1].price);
-// }
-
-// function clicked() {
-//   let p = createP(json[ floor(random(20)) ].title);
-//   p.position( random(displayWidth), random(displayHeight) )
-// }
-
-
 let json;
 let count = 0;
 let keysArray;
+let popUpCount = 0;
 
 function preload() {
   json = loadJSON("stuff.json", loaded);
@@ -54,13 +20,43 @@ function setup() {
 function displayProduct() {
   let product = json[floor(random(count))];
   let productDiv = createDiv();
+ 
+  if (popUpCount === 0) {
+    productDiv.style('background-color','white' ); 
+  } else {
+    productDiv.style('background-color','red' ); 
+    
+    let exitButton = createButton("X")
+    exitButton.mouseClicked(() => {
+      productDiv.remove()
+    } )
+    exitButton.parent(productDiv)
+
+    let confirmationMessage = createP("Are you sure? What about this one instead?");
+    confirmationMessage.parent(productDiv);
+    confirmationMessage.style('font-weight', 'bold');
+    confirmationMessage.style('font-family', 'Arial, sans-serif');
+
+  }
+
+  productDiv.style('padding', '20px');
+  productDiv.style('position', 'absolute');
+  productDiv.style('border', '2px solid black');
+  productDiv.center();
+  productDiv.position(random(windowWidth - 200), random(windowHeight - 200));
+  
   let productP = createP(product.title);
   productP.parent(productDiv);
   let img = createImg(product.image);
   img.parent(productDiv);
   img.style("width", "200px");
-  createP(product.price);
+  createP(product.price).parent(productDiv);
+  
   let buyButton = createButton("Buy Now");
-  buyButton.mouseClicked(displayProduct);
+  buyButton.mouseClicked(() => {
+    popUpCount++;
+    displayProduct();
+  });
+  buyButton.parent(productDiv);
 }
 
